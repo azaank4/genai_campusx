a0e8f643a87eec6e7865340a9b0d7a719a7c5b89
azaank4 <khanazaan1441@gmail.com>
2025-12-04 20:16:10 +0530

    Updated the terminal chatbot with chat history feature

--- langchain_prompts/chatbot.py
+++ langchain_prompts/chatbot.py
@@ -5,11 +5,16 @@ load_dotenv()
 
 model = ChatGoogleGenerativeAI(model = 'gemini-2.5-flash')
 
+chat_history = []
+
 while True:
     user_input = input("You: ")
+    chat_history.append(user_input)
     if user_input.lower() == "exit":
         print("Goodbye!")
         break
-    response = model.invoke(user_input)
+    response = model.invoke(chat_history)
+    chat_history.append(response.content)
     print(f"Bot: {response.content}")
-    
+
+print(chat_history)
