9d1e688876bf6ea73a1a55c5c12cb67170dbe8ef
azaank4 <khanazaan1441@gmail.com>
2025-12-05 16:49:11 +0530

    Structured output using TypedDict

---
+++ langchain_structured_outputs/pydantic_demo.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, EmailStr, Field
+# EmailStr is used for validating email format
+# Field is used for adding extra information/constraints to the fields in the model
+from typing import Optional
+
+class Student(BaseModel):
+    name : str = 'Mizba'
+    age : Optional[int] = None
+    email : EmailStr
+    cgpa : float = Field(gt = 0, lt = 10.1)
+# This '=' is used setting default value for the field
+
+new_student = {'age' : 20, 'email' : 'abc@gmail.com', 'cgpa' : 10}
+student = Student(**new_student)
+
+student_dict= dict(student)
+# We can convert the pydantic dictionary to python dictionary
+
+student_json = student.model_dump_json()
+# We can convert the pydantic model to json format
+print(student_dict)
+print(student_json)
